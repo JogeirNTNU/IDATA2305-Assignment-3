5dbc17e822b7d42bba8db0ed4aa32588204d63f7
Kristian <kristianty80@gmail.com>
2026-03-23 13:48:53 +0100

    Added getter for list

--- app/src/main/java/org/example/SCAN.java
+++ app/src/main/java/org/example/SCAN.java
@@ -1,18 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * SCAN disc scheduling algorithm.
  */
 public class SCAN {
+  private List<Integer> requests = new ArrayList<>();
 
   /**
    * Constructor.
    */
-  public SCAN(){
+  public SCAN(List<Integer> requests) {
+    this.requests = requests;
+  }
+
+  public List<Integer> getRequests() {
+    return this.requests;
+  }
 
 }
-
-
-
-}
-
