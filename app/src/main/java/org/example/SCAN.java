724ae1639db39f06db83c6a5bfbe3b530044e992
Håkon <haakmae@stud.ntnu.no>
2026-03-24 12:24:58 +0100

    implemented the SCAN algorithm

--- app/src/main/java/org/example/SCAN.java
+++ app/src/main/java/org/example/SCAN.java
@@ -1,23 +1,93 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import org.example.Needle;
 
 /**
  * SCAN disc scheduling algorithm.
  */
 public class SCAN implements Strategy {
-  private List<Integer> requests = new ArrayList<>();
+  private final Needle needle;
 
   /**
    * Constructor.
    */
-  public SCAN(List<Integer> requests) {
-    this.requests = requests;
+  public SCAN(Needle needle) {
+    this.needle = needle;
+
   }
 
-  public List<Integer> getRequests() {
-    return this.requests;
+
+
+
+
+  @Override
+  public Needle run() {
+
+    List<Integer> clockWise = new ArrayList<>();
+    List<Integer> antiClockWise = new ArrayList<>();
+
+
+
+    for (int i : needle.getRequests()) {
+      if (i < needle.getStartPos()) {
+        clockWise.add(i);
+      } else {
+        antiClockWise.add(i);
+      }
+    }
+
+    clockWise.sort(Comparator.reverseOrder());
+    antiClockWise.sort(Comparator.naturalOrder());
+
+
+
+    needle.addPathToNeedlePath(needle.getStartPos());
+    boolean isFinished = false;
+    while (!isFinished) {
+      if (needle.isClockwisePath()) {
+
+        for (int i : clockWise) {
+
+          needle.addPathToNeedlePath(i);
+          needle.addTotalHeadMovement(Math.abs(needle.getStartPos()) - needle.getCurrentPosition());
+        }
+
+        for (int i : antiClockWise) {
+
+          needle.addPathToNeedlePath(i);
+          needle.addTotalHeadMovement(Math.abs(antiClockWise.get(i) - needle.getCurrentPosition()));
+
+
+        }
+        isFinished = true;
+
+      } else {
+        for (int i : antiClockWise) {
+
+          needle.addPathToNeedlePath(i);
+          needle.addTotalHeadMovement(Math.abs(antiClockWise.get(i) - needle.getCurrentPosition()));
+
+
+        }
+        for (int i : clockWise) {
+
+          needle.addPathToNeedlePath(i);
+          needle.addTotalHeadMovement(Math.abs(needle.getStartPos()) - needle.getCurrentPosition());
+        }
+        isFinished = true;
+      }
+
+    }
+
+
+    return needle;
   }
 
+  @Override
+  public String getAlgorithmName() {
+    return "SCAN";
+  }
 }
